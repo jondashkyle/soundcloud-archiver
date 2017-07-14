779a86114ca0eeaaf346169efefd2ecdaa2c5901
jondashkyle <contact@jon-kyle.com>
2017-07-14 17:28:03 -0400

    cleanup

---
+++ cleanup.js
@@ -0,0 +1,45 @@
+var fs = require('fs')
+var path = require('path')
+var rimraf = require('rimraf')
+var toiletdb = require('toiletdb')
+var subtoilet = require('subtoilet')
+var db = toiletdb(path.join(__dirname, '.db.json'))
+
+db.open(function (err) {
+  if (err) console.warn(err.message)
+})
+
+// setup
+var archivesdb = subtoilet(db, 'archives')
+var now = Date.now()
+
+archivesdb.read(function (err, data) {
+  if (err) console.warn(err.message)
+  if (!data) return console.warn('no data')
+  Object.keys(data).forEach(function (key) {
+    var date = data[key].date
+    if (date) {
+      var timeDiff = Math.abs(new Date(now).getTime() - new Date(date).getTime())
+      var diff = Math.ceil(timeDiff / (1000 * 60))
+      // delete if it’s old
+      if (diff > 15) {
+        try {
+          rimraf.sync(path.join(__dirname, '.tracks/', key))
+        } catch (err) {
+          console.log('can not remove ' + key)
+        }
+      }
+    }
+    // readArchive
+  })
+})
+
+
+// .forEach(function (key) {
+//   var date = db.archives[key].date
+//   if (date) {
+//     console.log(now, date)
+//   } else {
+
+//   }
+// })
