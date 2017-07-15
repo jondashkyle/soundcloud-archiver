a48c1b1b00559f39b4bef3f1ff3a94c84e6f5220
jondashkyle <contact@jon-kyle.com>
2017-07-15 01:12:54 -0400

    delete based off finished date

--- cleanarchives.js
+++ cleanarchives.js
@@ -19,7 +19,7 @@ archivesdb.read(function (err, data) {
   if (err) console.warn(err.message)
   if (!data) return console.warn('no data')
   Object.keys(data).forEach(function (key) {
-    var date = data[key].date
+    var date = data[key].finished
     if (date) {
       var timeDiff = Math.abs(new Date(now).getTime() - new Date(date).getTime())
       var diff = Math.ceil(timeDiff / (1000 * 60))
@@ -32,6 +32,5 @@ archivesdb.read(function (err, data) {
         }
       }
     }
-    // readArchive
   })
 })
